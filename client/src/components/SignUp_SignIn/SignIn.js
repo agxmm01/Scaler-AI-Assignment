d24335287a38b19febdc42b6d88259b79335091a
Agampal Singh <agampalsingh61@gmail.com>
2026-03-29 15:47:10 +0530

    updated

--- client/src/components/SignUp_SignIn/SignIn.js
+++ client/src/components/SignUp_SignIn/SignIn.js
@@ -34,7 +34,7 @@ const SignIn = () => {
     }
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/login`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
